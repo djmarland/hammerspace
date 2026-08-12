98f02018a55642a63bbd30302ae520c2ca64ac87
David Marland <davidm@rland.me.uk>
2026-08-12 22:35:56 +0100

    SvelteKit: align deps and fix svelte.config.js for kit v2/svelte v5

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
-  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     csrf: {
